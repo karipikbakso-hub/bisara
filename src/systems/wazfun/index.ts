5b6872fd71130b364cb01fcce1ca44b215c8f170
karipik.bakso <karipik.bakso@gmail.com>
2025-12-26 13:36:18 +0700

    Update mesin cuci

--- src/systems/wazfun/index.ts
+++ src/systems/wazfun/index.ts
@@ -1,7 +1,6 @@
 // WAZFUN System Exports
 export { default as Dashboard } from './pages/dashboard';
 export { default as Operator } from './pages/operator';
-export { MachineCard } from './components/MachineCard';
 export { WazfunHero } from './components/WazfunHero';
 
 // System Configuration
@@ -9,7 +8,6 @@ export { default as config } from './config.json';
 
 // Components export
 export const components = {
-  MachineCard: () => import('./components/MachineCard').then(m => ({ default: m.MachineCard })),
   WazfunHero: () => import('./components/WazfunHero').then(m => ({ default: m.WazfunHero })),
 };
 
